dbc3b4cd4b3ade2d5a126ad9c95c5b9bb7fd35bf
birdofartemis <72250724+birdofartemis@users.noreply.github.com>
2021-12-22 19:01:07 -0100

    Design list of options on Config Module, corrections on SCSS files and correcting English to Portuguese

--- src/app/modules/config/config.module.ts
+++ src/app/modules/config/config.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { ConfigPanelComponent } from './components/config-panel/config-panel.component';
 import { ConfigRoutingModule } from './config-routing.module';
 
 @NgModule({
   declarations: [ConfigPanelComponent],
-  imports: [CommonModule, ConfigRoutingModule]
+  imports: [CommonModule, ConfigRoutingModule, MatListModule, MatSlideToggleModule]
 })
 export class ConfigModule {}
